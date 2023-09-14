483d8f5a3652d1a124c74e1c082620939d6705ea
Anastasia Ravossel <velnast@gmail.com>
2023-09-14 13:09:36 +0300

    feat: fiexed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
 import "./css/style.css";
+import mail from "../../img/mail.svg";
+import tel from "../../img/tel.svg";
+
 
 export const Footer = () => {
   return (
@@ -6,11 +9,11 @@ export const Footer = () => {
       <div className="container">
         <div className="contacts">
           <a className="contacts-link" href="mailto:info@dmitry@gmail.com">
-            <img className="contacts-icon" src="../../img/mail.svg" alt="" />
+            <img className="contacts-icon" src={mail} alt="" />
             info@dmitry@gmail.com
           </a>
           <a className="contacts-link" href="tel:+37125521551">
-            <img className="contacts-icon" src="../../img/tel.svg" alt="" />
+            <img className="contacts-icon" src={tel} alt="" />
             +371 255-21-551
           </a>
         </div>
